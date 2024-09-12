193f617345d836e4ff5c965fcd9b7845e284ee07
Shawn Tapps <stapps.prsvr@gmail.com>
2024-09-12 07:09:53 -0500

    working

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Time Section - maybe us another js file for wx widget and use import/export module?//
+
 let now
 
 function masterClock(){
@@ -84,3 +86,137 @@ updateClockGeo1();
 updateClockGeo2();
 updateClockGeo3();
 updateClockGeo4();
+
+//*************Weather widget sections below**************************************** */
+
+const apiKey = "752a802622ba8d12cd6c6d18438a7291";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=";
+const weatherIcon1 = document.getElementById("weather-icon1");
+const weatherIcon2 = document.getElementById("weather-icon2");
+const weatherIcon3 = document.getElementById("weather-icon3");
+const weatherIcon4 = document.getElementById("weather-icon4");
+
+async function checkWeather1(city) {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=nashville&appid=" + `${apiKey}`);
+
+
+    if (response.status == 404 || response.status == 401 || response.status == 400) {
+        document.getElementById("temp1").textContent = "Error";
+        document.getElementById("wind1").textContent = "Error";
+    } else {
+        var data = await response.json();
+
+        document.getElementById("temp1").textContent = Math.round(data.main.temp) + "° F";
+        document.getElementById("wind1").textContent = Math.round(data.wind.speed) + " MPH";
+            //CyLuvsKat//
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon1.src = "./assets/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon1.src = "./assets/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon1.src = "./assets/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon1.src = "./assets/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon1.src = "./assets/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon1.src = "./assets/snow.png";
+        }
+    }
+}
+
+async function checkWeather2(city) {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=london&appid=" + `${apiKey}`);
+
+
+    if (response.status == 404 || response.status == 401 || response.status == 400) {
+        document.getElementById("temp2").textContent = "Error";
+        document.getElementById("wind2").textContent = "Error";
+    } else {
+        var data = await response.json();
+
+        document.getElementById("temp2").textContent = Math.round(data.main.temp) + "° F";
+        document.getElementById("wind2").textContent = Math.round(data.wind.speed) + " MPH";
+            //CyLuvsKat//
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon2.src = "./assets/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon2.src = "./assets/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon2.src = "./assets/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon2.src = "./assets/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon2.src = "./assets/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon2.src = "./assets/snow.png";
+        }
+    }
+}
+
+async function checkWeather3(city) {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=New%20York&appid=" + `${apiKey}`);
+
+
+    if (response.status == 404 || response.status == 401 || response.status == 400) {
+        document.getElementById("temp3").textContent = "Error";
+        document.getElementById("wind3").textContent = "Error";
+    } else {
+        var data = await response.json();
+
+        document.getElementById("temp3").textContent = Math.round(data.main.temp) + "° F";
+        document.getElementById("wind3").textContent = Math.round(data.wind.speed) + " MPH";
+            //CyLuvsKat//
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon3.src = "./assets/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon3.src = "./assets/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon3.src = "./assets/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon3.src = "./assets/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon3.src = "./assets/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon3.src = "./assets/snow.png";
+        }
+    }
+}
+
+async function checkWeather4(city) {
+    const response = await fetch("https://api.openweathermap.org/data/2.5/weather?units=imperial&q=New%20Delhi&appid=" + `${apiKey}`);
+
+
+    if (response.status == 404 || response.status == 401 || response.status == 400) {
+        document.getElementById("temp4").textContent = "Error";
+        document.getElementById("wind4").textContent = "Error";
+    } else {
+        var data = await response.json();
+
+        document.getElementById("temp4").textContent = Math.round(data.main.temp) + "° F";
+        document.getElementById("wind4").textContent = Math.round(data.wind.speed) + " MPH";
+            //CyLuvsKat//
+        if (data.weather[0].main == "Clouds") {
+            weatherIcon4.src = "./assets/clouds.png";
+        } else if (data.weather[0].main == "Clear") {
+            weatherIcon4.src = "./assets/clear.png";
+        } else if (data.weather[0].main == "Rain") {
+            weatherIcon4.src = "./assets/rain.png";
+        } else if (data.weather[0].main == "Drizzle") {
+            weatherIcon4.src = "./assets/drizzle.png";
+        } else if (data.weather[0].main == "Mist") {
+            weatherIcon4.src = "./assets/mist.png";
+        } else if (data.weather[0].main == "Snow") {
+            weatherIcon4.src = "./assets/snow.png";
+        }
+    }
+}
+document.addEventListener('DOMContentLoaded', checkWeather1());
+document.addEventListener('DOMContentLoaded', checkWeather2());
+document.addEventListener('DOMContentLoaded', checkWeather3());
+document.addEventListener('DOMContentLoaded', checkWeather4());
+
+setInterval(checkWeather1, 300000);
+setInterval(checkWeather2, 300000);
+setInterval(checkWeather3, 300000);
+setInterval(checkWeather4, 300000);
